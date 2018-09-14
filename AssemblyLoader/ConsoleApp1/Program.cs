1fc56f424c81566e59faa264fe600d3a8a9103f6
Pietro Cattaneo <pieca314@gmail.com>
2018-09-14 13:58:43 +0200

    Primo Commit

---
+++ AssemblyLoader/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Assembly loadedAssembly = Assembly.LoadFrom("MyDll.dll");
+            Type customerType = loadedAssembly.GetType("MyDll.Customer");
+            var customerInstance = Activator.CreateInstance(customerType);
+            MethodInfo getFullName = customerType.GetMethod("getFullNameAndAge");
+            var parameters = new object[1];
+            parameters[0] = 23;
+            string nameAndAge = (string)getFullName.Invoke(customerInstance, parameters);
+
+            Console.WriteLine(nameAndAge);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AssemblyLoader/MyDll/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDll
+{
+    public class Customer
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+
+        public string getFullNameAndAge(int age)
+        {
+            return name + surname + age.ToString();
+        }
+    }
+}
